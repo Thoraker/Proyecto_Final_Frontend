2b301053605e035093e7a8afa5368bf3264a6f52
Félix Antonio Pacheco Lobos <pachecolobos.felix@gmail.com>
2023-06-28 20:39:33 -0400

    ajustes

--- src/components/addressForm.jsx
+++ src/components/addressForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useContext } from 'react'
-import PhotoUploader from './photoUploader'
 import { AppContext } from '../routes/App'
 
 const AddressForm = () => {
@@ -113,7 +112,6 @@ const AddressForm = () => {
 					<button type='submit' className='btn btn-primary'>
 						Publicar
 					</button>
-					<PhotoUploader />
 				</div>
 			</form>
 		</div>
