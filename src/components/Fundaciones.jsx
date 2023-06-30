d832dcdd47aa2382633dd1bb149a71363cb05281
Beverly M <124820505+66006600@users.noreply.github.com>
2023-06-30 17:33:15 -0400

    Invite, landing, modal listas

--- src/components/Fundaciones.jsx
+++ src/components/Fundaciones.jsx
@@ -32,55 +32,34 @@ const FundacionesDeAdopcion = () => {
     const tarjetas = fundaciones.map((fundacion, index) => (
         <Col key={index} >
             <Card className="shadow-lg d-flex h-100 p-2" style={{ backgroundColor: '#dbe6de' }}>
-                <Card.Img variant="top"  src={fundacion.foto} />
+                <Card.Img variant="top" src={fundacion.foto} />
                 <Card.Body>
                     <Card.Title>{fundacion.nombre}</Card.Title>
                     <Card.Text>{fundacion.descripcion}</Card.Text>
                     <Button variant="success" href={fundacion.paginaWeb} target="_blank">
                         Sitio Web
                     </Button>
+                    
                 </Card.Body>
             </Card>
         </Col>
     ));
 
     return (
-        <Container className="Cards p-4" style={{
-            background: 'rgb(180, 222, 191)',
+        <Container className="Cards p-4" style={{            
             background: 'linear-gradient(90deg, rgba(180, 222, 191, 0.6168592436974789) 19%, rgba(126, 148, 84, 1) 71%)'
         }}>
 
             <div>
-                <h1 className="text-center mb-4"> Fundaciones <RiHomeSmileFill /> </h1>
+                <h1 className="text-center mb-4"> Fundaciones </h1>
                 <h5> Aquí te mostramos las fundaciones en alianza con nuestra aplicación donde podrás conocer más a fondo la labor de cada una de estas organizaciones dedicadas al rescate, rehabilitación, cuidado, reubicación y adopcion de mascotas abandonadas.
-                    Cada mascota tiene su historia 
-	return (
-		<Container
-			className='Cards'
-			style={{
-				background: 'rgb(180, 222, 191)',
-				background:
-					'linear-gradient(90deg, rgba(180, 222, 191, 0.6168592436974789) 19%, rgba(126, 148, 84, 1) 71%)',
-			}}
-		>
-			<div>
-				<h1 className='text-center m-4'>
-					{' '}
-					Fundaciones <i className='bi bi-house-door-fill'></i>{' '}
-				</h1>
-				<h5>
-					{' '}
-					Aquí te mostramos las fundaciones en alianza con nuestra aplicación donde podrás conocer más a fondo
-					la labor de cada una de estas organizaciones dedicadas al rescate, rehabilitación, cuidado,
-					reubicación y adopcion de mascotas abandonadas. Cada mascota tiene su historia particular y todos
-					necesitan tu aporte. Puedes ser voluntario, realizar aportes monetarios, difundir la pagina de la
-					fundación, contactarte con ellos entre otros.{' '}
-				</h5>
-			</div>
+                    Cada mascota tiene su historia</h5>
+            </div>
 
-			<Row className='h-100 align-items-stretch'>{tarjetas}</Row>
-		</Container>
-	)
-}
+            <Row className='h-100 align-items-stretch'>{tarjetas}</Row>
+        </Container>
+    )
+
+};
 
 export default FundacionesDeAdopcion
